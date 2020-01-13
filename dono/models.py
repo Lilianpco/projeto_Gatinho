54b4f1e91db6e9b04d3a7d979867de733a638cb2
Lilian <lilian.cascardo@outlook.com>
2020-01-13 14:21:11 -0300

    primeiro commit

--- dono/models.py
+++ dono/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here
 class Dono(models.Model):
     nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=225)
+    endereco = models.CharField(max_length=200)
 
     def __str__(self):
         return self.nome
