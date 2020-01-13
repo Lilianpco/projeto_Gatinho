24e554556844b678cc9c84357ef1c87a24c7cedb
Lilian <lilian.cascardo@outlook.com>
2020-01-13 16:31:43 -0300

    alteracao projeto

--- dono/views.py
+++ dono/views.py
@@ -13,3 +13,37 @@ def cadastro(request):
         form.save()
         args['msg'] = 'Cadastro Realizado com sucesso'
     return render(request, 'cadastro.html', args)
+
+    
+def detail(request, id):
+    dono = Dono.objects.get(id=id)
+    donos = gatinho.dono.all()
+    args = {
+        'donos':donos,
+        'dono':donos
+    }
+    return render(request, 'detail.html', args)
+
+def delete(request, id):
+    dono = Dono.objects.get(id=id)
+
+    args = {
+        dono': dono
+    }
+
+    dono.delete()
+    return render(request, 'delete.html', args)
+
+def update(request, id):
+    dono = Dono.objects.get(id=id)
+    form =DonoForm(request.POST or None, instance=dono)
+
+    if form.is_valid():
+        form.save()
+        return redirect(f'../detail/{dono.id}')
+
+    args = {
+        'dono':dono,
+        'form':form
+    }
+    return render(request, 'update.html', args)
