d9dcf8cd8e85067c250e7b4c4b4bb2efd169a4b3
Lilian <lilian.cascardo@outlook.com>
2020-01-15 13:48:49 -0300

    adição de atributos à app cachorro

--- cachorros/models.py
+++ cachorros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Cachorro(models.Model):
+    nome = models.CharField(max_length=255)
+    idade =models.IntegerField() 
+    cor_do_pelo = models.CharField(max_length=255)
+    porte = models.CharField(max_length=255)
+    dono = models.CharField(max_length=255)
+    
+
 
 # Create your models here.
